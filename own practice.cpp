faed18a53e116b3903661d95ee80c2531eec68b7
Nashia Hassan <168549847+Nashia10@users.noreply.github.com>
2024-06-01 00:35:49 +0600

    Add files via upload

--- own practice.cpp
+++ own practice.cpp
@@ -1,11 +1,3 @@
-/* print the given pattern
-if n=5
-1
-AB
-123
-ABCD
-12345*/
-
 #include<iostream>
 using namespace std;
 int main(){
@@ -15,12 +7,8 @@ cout<<"Enter a value for n:";
 cin>>n;
 
 for(int i=1; i<=n;i++){//for rows
-        for(int j=1; j<=i; j++){
-            if(i%2!=0){
-                cout<<j<<" ";
-            }else{
-                cout<<char(j+64)<<" ";
-            }
+        for(int j=i;j<=n; j++){
+            cout<<"*"<<" ";
         }
         cout<<endl;
 
