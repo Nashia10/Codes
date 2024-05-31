d98ce599241f82eec00f13e574c4b116f1d5132d
Nashia Hassan <168549847+Nashia10@users.noreply.github.com>
2024-06-01 00:11:37 +0600

    Add files via upload

---
+++ own practice.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cout<<"Enter a value for n:";
+cin>>n;
+
+for(int i=1; i<=n;i++){//for rows
+        for(int j=1; j<=i; j++){
+            if(i%2!=0){
+                cout<<j<<" ";
+            }else{
+                cout<<char(j+64)<<" ";
+            }
+        }
+        cout<<endl;
+
+}
+
+
+
+}
